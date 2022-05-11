6afcf050058e9d64b578559a72368c7eae8a037c
Lykourgos Tsirikos <4200197+ltdev22@users.noreply.github.com>
2022-05-11 09:14:45 +0100

    Fix zshrc symlink on install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ echo "Setting up your MacOS..."
 
 # Remove .zshrcfrom home directory, if it exists, and create a symlink of the .zshrc file from the .dotfiles directory into home
 rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # .. same for .gitconfig
 rm -rf $HOME/.gitconfig
 ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
